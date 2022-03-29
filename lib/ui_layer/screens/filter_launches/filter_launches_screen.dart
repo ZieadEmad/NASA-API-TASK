8a9f5ceb74234ccd0348da5b1fb2b961b3be0fe4
ZieadEmad <zieadwjsjs@gmail.com>
2022-03-29 15:29:53 +0200

    abdo task

--- lib/ui_layer/screens/filter_launches/filter_launches_screen.dart
+++ lib/ui_layer/screens/filter_launches/filter_launches_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mega_task/main.dart';
 
 class FilterLaunchesScreen extends StatelessWidget {
   const FilterLaunchesScreen({Key? key}) : super(key: key);
@@ -7,6 +8,324 @@ class FilterLaunchesScreen extends StatelessWidget {
     return Column(
       children: [
         Center(child: Text('3'),),
+        SizedBox(height: 100,),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height/10,
+          decoration: BoxDecoration(
+            color: Colors.grey[200]
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(18 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(19 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(20 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(19 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(18 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(17 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(16 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              RotationTransition(
+                turns:  AlwaysStoppedAnimation(15 / 360),
+                child: Container(
+                  width: 5,
+                  height: MediaQuery.of(context).size.height/10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+
+                  ),
+                ),
+              ),
+              SizedBox(width: 4,),
+              Align(
+                alignment: AlignmentDirectional.bottomEnd,
+                child: RotationTransition(
+                  turns:  AlwaysStoppedAnimation(14 / 360),
+                  child: Container(
+                    width: 5,
+                    height: MediaQuery.of(context).size.height/15,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+
+                    ),
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        )
+
       ],
     );
   }
